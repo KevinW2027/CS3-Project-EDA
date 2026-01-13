1c67de23d07eabc8e405daa41206a27763851797
KevinW2027 <kevin.wang2027@gbwl.org>
2026-01-13 19:36:55 +0000

    commit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pandas as pd
 from matplotlib import pyplot as plt
-
-imp = pd.read_csv('AI_Impact_on_Jobs_2030.csv')
+import seaborn as sns 
+df = pd.read_csv('AI_Impact_on_Jobs_2030.csv')
 #print(imp.info)
-imp = imp.iloc[:, :-10]
-print(imp.columns)
-print(imp.head(8))
+df = df.iloc[:, :-10]
+print(df.columns)
+print(df.head(8))
