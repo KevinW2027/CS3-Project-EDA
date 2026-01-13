7de3b4d5f430344be7cefd32e2315a61b1e8494b
KevinW2027 <kevin.wang2027@gbwl.org>
2026-01-13 19:55:03 +0000

    commit

--- main.py
+++ main.py
@@ -6,3 +6,16 @@ df = pd.read_csv('AI_Impact_on_Jobs_2030.csv')
 df = df.iloc[:, :-10]
 print(df.columns)
 print(df.head(8))
+
+sns.set_theme(style='whitegrid', context='notebook')
+
+#1 Boxplot /  Violinplot
+edu_order  = sorted(df["Education_Level"].unique())
+sns.violinplot(
+    data=df,
+    x= "Education level",
+    y= "Automation Prob 2030",
+    order= edu_order,
+    inner= "quartile",
+    cut= 0
+)
